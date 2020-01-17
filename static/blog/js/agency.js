3fb8e24a3c1b2db3428741e66b11b0ea5dd4c733
viktor.orkush <viktor.orkush@gmail.com>
2020-01-17 16:56:53 +0200

    edit photo gallery prepare for release

--- static/blog/js/agency.js
+++ static/blog/js/agency.js
@@ -27,34 +27,6 @@ $(function () {
 });
 // END gallery
 
-// Тип Image - галерея картинок
-$('.popup-gallery').magnificPopup({
-    delegate: 'a',
-    type: 'image',
-    tLoading: 'Загрузка изображения #%curr%...',
-    mainClass: 'mfp-img-mobile',
-    image: {
-        verticalFit: true,
-        titleSrc: function (item) {
-            return item.el.attr('title') + ' &middot; <a class="image-source-link" href="' + item.el.attr('data-source') + '" target="_blank">image source</a>';
-        }
-    },
-    gallery: {
-        enabled: true,
-        navigateByImgClick: true,
-        preload: [0, 1] // Will preload 0 - before current, and 1 after the current image
-    },
-    zoom: {
-        enabled: true,
-        duration: 300, // don't foget to change the duration also in CSS
-        opener: function (element) {
-            return element.find('img');
-        }
-    }
-});
-
-// endgallery
-
 
 (function ($) {
     "use strict"; // Start of use strict
@@ -98,35 +70,3 @@ $('.popup-gallery').magnificPopup({
     $(window).scroll(navbarCollapse);
 
 })(jQuery); // End of use strict
-
-
-
-
-
-
-$(document).ready(function() {
-	$('.zoom-gallery').magnificPopup({
-		delegate: 'a',
-		type: 'image',
-		closeOnContentClick: false,
-		closeBtnInside: false,
-		mainClass: 'mfp-with-zoom mfp-img-mobile',
-		image: {
-			verticalFit: true,
-			titleSrc: function(item) {
-				return item.el.attr('title') + ' &middot; <a class="image-source-link" href="'+item.el.attr('data-source')+'" target="_blank">image source</a>';
-			}
-		},
-		gallery: {
-			enabled: true
-		},
-		zoom: {
-			enabled: true,
-			duration: 300, // don't foget to change the duration also in CSS
-			opener: function(element) {
-				return element.find('img');
-			}
-		}
-
-	});
-});
